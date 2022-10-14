5752029201a58a5ef67732223f65d04da1be698e
Darkerny37 <secreto_archivo@hotmail.com>
2022-10-14 16:13:17 -0600

    Boton que pasa a otro activity

---
+++ app/src/main/java/com/example/practicaley/AgregarTarea.java
@@ -0,0 +1,14 @@
+package com.example.practicaley;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AgregarTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recibidor_menu);
+    }
+}
---
+++ app/src/main/java/com/example/practicaley/MenuRecibidor.java
@@ -0,0 +1,15 @@
+package com.example.practicaley;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuRecibidor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recibidor_menu);
+    }
+}
