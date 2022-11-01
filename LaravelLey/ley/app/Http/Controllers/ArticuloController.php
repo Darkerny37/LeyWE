cca45c14472d121967c1af7f95ad207084db5d7a
Jesus Armando Carrizoza Tamayo <armandoxd_f.o.b@hotmail.com>
2022-10-31 22:56:45 -0700

    Laravel
    Login y Agregar Articulo

---
+++ LaravelLey/ley/app/Http/Controllers/ArticuloController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Articulo;
+
+class ArticuloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // art es un objeto de la base de datos
+        $art = new Articulo();
+
+        // si es diferente de nulo tiene valores
+        if( $request->nombreArticulo !==null and
+            $request->numeroSerie !==null and
+            $request->cantidad !==null)
+        {
+            // los atributos de art deben ser iguales a los de la bd
+            // se asignan valores del request
+            $art->nombreArticulo = $request->nombreArticulo;
+            $art->numeroSerie = $request->numeroSerie;
+            $art->cantidad = $request->cantidad;
+
+            // de esta forma los cambios quedan guardados
+            if($art->save())
+            {
+                return response()->json([
+                    'message' => 'Registro guardado correctamente'
+                ]);
+            }else{
+                return response()->json([
+                    'message' => 'No se guardo el registro'
+                ]);
+            }
+        }else{
+            return response()->json([
+                'message' => 'Campos nulos'
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
